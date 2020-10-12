63bd82d4914704bac2b6ec5a642bc98724b3f361
Jay Patel <jaypatel32157@gmail.com>
2020-10-12 19:35:33 +0530

    First Commit

---
+++ rentpay.py
@@ -0,0 +1,4 @@
+hours = input("Total Hours :")
+rent = input("Rent per Hour :")
+pay = float(hours)*float(rent)
+print("Total Amount :"+ str(pay))
---
+++ var_exp.py
@@ -0,0 +1,15 @@
+# comment in the python
+# fetching the name of tht user
+fname = input('Please Enter Your First Name :')
+lname = input('Please Enter Your Last Name :')
+greet = "Welcome " + lname + " " + fname + "."
+print(greet)
+
+#playing around with Exp
+
+num1 = 20
+num2 = 5
+div = num1/num2
+print(div)
+num1 = num1+1
+print(num1)
