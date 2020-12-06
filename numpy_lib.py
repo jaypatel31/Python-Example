9eee99af1062abb71ba4f96cd7c42376b7387aeb
Jay Patel <jaypatel32157@gmail.com>
2020-12-06 20:42:11 +0530

    Numpy_libraray

--- numpy_lib.py
+++ numpy_lib.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-x = np.array([["Jay","Ketul","Jimmy"],[10,5,16]])
+x = np.array([["Jay","Ketul","Jimmy","Gohil"],[10,5,16,50]])
 
 print("Array:\n",x)
 print("Array is",x.ndim,"dimensions")
@@ -16,3 +16,13 @@ print()
 print("{",x[0,1:],"}","=","{",x[1,1:],"}")
 print()
 print("Data Type Of Array",x.dtype)
+cparr = x.copy()#Copy Of Array x
+vwarr = x.view()#View Of Array x
+print() 
+print("Shape of Array:",x.shape)#Number of Element Present In Each Dimensons
+resparr = x.reshape(2,2,2)
+print()
+print("Reshaped Into 3-d:\n",resparr)
+print()
+for idx,elem in np.ndenumerate(x[0]):
+    print(elem,"=",x[1,idx[0]]) 
