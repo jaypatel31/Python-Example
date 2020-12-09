f1e32bf032542a0a585f9689b46616fbc9559557
Jay Patel <jaypatel32157@gmail.com>
2020-12-09 11:08:45 +0530

    Scipy_Lib PY

---
+++ scipy_lib.py
@@ -0,0 +1,10 @@
+from scipy import constants
+
+print("1 year =",constants.year,"s")#Return in Seconds
+print("1 centimeter =",constants.centi,"m")#Return in Meter
+print("1 KB =",constants.kibi,"Bytes")
+print("1 Bar =",constants.bar,"Pascal")  
+print("1 km/s =",constants.kmh,"m/s") 
+print("1 eV =",constants.eV,"Joules")
+print("1 HP =",constants.hp,"Watt") 
+print("1 Dyne =",constants.dyn,"Newton")
