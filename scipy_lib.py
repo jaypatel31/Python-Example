5449d5b4274378b242be62832ada74339ad64014
Jay Patel <jaypatel32157@gmail.com>
2020-12-11 19:33:42 +0530

    Scipy_lib

--- scipy_lib.py
+++ scipy_lib.py
@@ -1,4 +1,6 @@
 from scipy import constants
+import numpy as np
+from scipy.sparse import csr_matrix
 
 print("1 year =",constants.year,"s")#Return in Seconds
 print("1 centimeter =",constants.centi,"m")#Return in Meter
@@ -8,3 +10,11 @@ print("1 km/s =",constants.kmh,"m/s")
 print("1 eV =",constants.eV,"Joules")
 print("1 HP =",constants.hp,"Watt") 
 print("1 Dyne =",constants.dyn,"Newton")
+
+arr = np.array([[0, 0, 0, 2], [0, 0, 1, 0], [1, 0, 2,0]])
+
+print(csr_matrix(arr))
+print()
+print("Non - Zero Data:",csr_matrix(arr).data)
+print()
+print("Total Non - Zero Data:",csr_matrix(arr).count_nonzero())
