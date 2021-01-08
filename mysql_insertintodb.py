5440188e1a407c061a9bd224bcaeacf291c255be
Jay Patel <jaypatel32157@gmail.com>
2021-01-08 17:44:57 +0530

    Mulitple Insertion

--- mysql_insertintodb.py
+++ mysql_insertintodb.py
@@ -8,10 +8,20 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+ip = input("Enter The Number of Students: ")
+ip = int(ip)
+multi = list()
+for x in range(ip):
+    name = input("Enter The Name of Student: ")
+    grd = input("Enter The Class of Student: ")
+    mrk = input("Enter The Marks of Student: ")
+    tmplist = (name,grd,mrk)
+    multi.append(tmplist)
 
-sql = "INSERT INTO student_info (student_name,student_class,student_marks) VALUES ('Jay',12,89.5)"
+sql = "INSERT INTO student_info (student_name,student_class,student_marks) VALUES (%s, %s, %s)"
 
-mycursor.execute(sql)
+
+mycursor.executemany(sql, multi)
 mydb.commit()
 
 print(mycursor.rowcount, "record inserted.")
